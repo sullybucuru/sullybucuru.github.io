5c2602be8c552e84b8fa01c075e621de0d56fcd7
sullybucuru <89140336+sullybucuru@users.noreply.github.com>
2021-08-29 23:43:15 -0500

    Actualizacion Pagina Web

---
+++ js/script.js
@@ -0,0 +1,11 @@
+$(document).ready(function(){
+    $("#boton").click(function(evento){
+        alert("Hola, soy el click");
+    })
+})
+function enviar(){
+    alert("Vas a enviar un mensaje")
+    $(".footer").addClass("divfooter");
+    setTimeout
+    $(".form").addClass("ver");
+};
